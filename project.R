237dcfb230eb96bf4676d64f6505606c7ed3e0ca
bouldercoder9 <alma9011@colorado.edu>
2020-10-15 13:50:52 -0600

    finished d

--- project.R
+++ project.R
@@ -40,6 +40,7 @@ view_Top_Occurences <- function(corpus)
   return(sorted)
 }
 
+#Return the lowest ten sentiment novels
 view_Bing_Sentiment <- function(corpus)
 {
   temp <- corpus%>%
@@ -47,7 +48,23 @@ view_Bing_Sentiment <- function(corpus)
     count(index = gutenberg_id, sentiment)%>%
     spread(sentiment, n)%>%
     mutate(sentiment = positive - negative)%>%
+    top_n(-10)%>%
     arrange(sentiment, .by_group = TRUE)
+  return(temp)
+}
+
+#Return the top 15 negative and positive words in a corpora
+get_Contributing_Negative_Words <- function(corpus)
+{
+  temp <- corpus%>%
+    inner_join(BING)%>%
+    count(word, sentiment)%>%
+    group_by(sentiment)%>%
+    top_n(15)%>%
+    ungroup()%>%
+    mutate(word = reorder(word,n))%>%
+    group_by(sentiment)%>%
+    arrange(desc(n), .by_group = TRUE)
   return(temp)
 }
 
@@ -59,13 +76,14 @@ view_Bing_Sentiment <- function(corpus)
 american_corpus <- clean(download_src(american_id))
 uk_corpus <- clean(download_src(uk_id))
 
-
 #View each corpus
 american_words <- view_Top_Occurences(american_corpus)
 uk_words <- view_Top_Occurences(uk_corpus)
   
+#Create each corpus sentiment
 american_sentiment <- view_Bing_Sentiment(american_corpus)
 uk_sentiment <- view_Bing_Sentiment(uk_corpus)
 
-american_corpus <- clean(download_src(american_id))
-uk_corpus <- clean(download_src(uk_id))
+#View top 15 contributing words to BING sentiment
+american__word_sentiment <- get_Contributing_Negative_Words(american_corpus)
+uk_word_sentiment <- get_Contributing_Negative_Words(uk_corpus)
