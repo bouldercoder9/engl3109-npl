9c7125c532682cd4b7d1276bb98df78433345dab
bouldercoder9 <alma9011@colorado.edu>
2020-10-15 14:04:48 -0600

    fixed

--- project.R
+++ project.R
@@ -51,6 +51,21 @@ view_Bing_Sentiment <- function(corpus)
   return(temp)
 }
 
+#Return the top 15 negative and positive words in a corpora
+get_Contributing_Negative_Words <- function(corpus)
+{
+  temp <- corpus%>%
+    inner_join(BING)%>%
+    count(word, sentiment)%>%
+    group_by(sentiment)%>%
+    top_n(15)%>%
+    ungroup()%>%
+    mutate(word = reorder(word,n))%>%
+    group_by(sentiment)%>%
+    arrange(desc(n), .by_group = TRUE)
+  return(temp)
+}
+
 ###########################
 ## Corpus Work
 ###########################
@@ -59,19 +74,19 @@ view_Bing_Sentiment <- function(corpus)
 american_corpus <- clean(download_src(american_id))
 uk_corpus <- clean(download_src(uk_id))
 
-
-#View each corpus
+#Create top word occurences
 american_words <- view_Top_Occurences(american_corpus)
 uk_words <- view_Top_Occurences(uk_corpus)
-  
+
+#Create corpus sentiments
 american_sentiment <- view_Bing_Sentiment(american_corpus)
 uk_sentiment <- view_Bing_Sentiment(uk_corpus)
-
-american_corpus <- clean(download_src(american_id))
-uk_corpus <- clean(download_src(uk_id))
 
 # To find the total sentiment we calculated the sum of the sentiment columns that we generated earlier 
 # To achieve this we are using the sum function and then specifying which variable to pull from and using the $ to specify which column to pull from for the sums
 american_total_sentiment = sum(american_sentiment$sentiment)
 uk_total_sentiment = sum(uk_sentiment$sentiment)
 
+#View top 15 contributing words to BING sentiment
+american__word_sentiment <- get_Contributing_Negative_Words(american_corpus)
+uk_word_sentiment <- get_Contributing_Negative_Words(uk_corpus)
