ca62b4d591fec87f52c469674ed33d3399706a22
Alex Mazur <alma9011@colorado.edu>
2020-10-19 16:49:20 -0600

    Finished code (no ggplot)

--- project.R
+++ project.R
@@ -8,7 +8,7 @@ library(textdata)
 #######################
 
 #Create an American and UK list of book ID's
-american_id <- scan("american_corpus")
+us_id <- scan("american_corpus")
 uk_id <- scan("uk_corpus")
 
 #Create our stop words SMART and BING set
@@ -40,10 +40,10 @@ view_Top_Occurences <- function(corpus)
   return(sorted)
 }
 
-view_Bing_Sentiment <- function(corpus)
+get_Sentiment <- function(corpus, lexicon)
 {
   temp <- corpus%>%
-    inner_join(BING)%>%
+    inner_join(lexicon)%>%
     count(index = gutenberg_id, sentiment)%>%
     spread(sentiment, n)%>%
     mutate(sentiment = positive - negative)%>%
@@ -66,70 +66,94 @@ get_Contributing_Negative_Words <- function(corpus)
   return(temp)
 }
 
+#Create a list of the top 15 joy words in a corpus
+joy_Count <- function(corpus, joy_list)
+{
+  temp <- corpus%>%
+    inner_join(joy_list)%>%
+    count(word, sort = TRUE)%>%
+    top_n(15)
+  return(temp)
+}
+
+#Create a list of the top 15 anger words in a corpus
+anger_Count <- function(corpus, anger_list)
+{
+  temp <- corpus%>%
+    inner_join(anger_list)%>%
+    count(word, sort = TRUE)%>%
+    top_n(15)
+  return(temp)
+}
+
+get_Top_Disgust <- function(corpus, disgust_list)
+{
+  temp <- corpus%>%
+    inner_join(disgust_list)%>%
+    count(index = gutenberg_id)%>%
+    arrange(desc(n), .by_group = TRUE)%>%
+    top_n(1)
+  return(temp)
+}
+
+#Gets the AFINN sentiment of a corpus
+get_AFINN_Sentiment <- function(corpus, lexicon)
+{
+  temp <- corpus%>%
+    inner_join(lexicon)%>%
+    group_by(gutenberg_id)%>%
+    summarise(sentiment = sum(value))%>%
+    arrange(sentiment, .by_group = TRUE)
+  return(temp)
+}
+
 ###########################
 ## Corpus Work
 ###########################
 
 #Use the above functions to create our two corpus's
-american_corpus <- clean(download_src(american_id))
+us_corpus <- clean(download_src(us_id))
 uk_corpus <- clean(download_src(uk_id))
-
 
 #View each corpus
-american_words <- view_Top_Occurences(american_corpus)
+us_words <- view_Top_Occurences(us_corpus)
 uk_words <- view_Top_Occurences(uk_corpus)
   
-american_sentiment <- view_Bing_Sentiment(american_corpus)
-uk_sentiment <- view_Bing_Sentiment(uk_corpus)
-
-american_corpus <- clean(download_src(american_id))
-uk_corpus <- clean(download_src(uk_id))
+#View the bing sentiment
+us_BING_sentiment <- get_Sentiment(us_corpus, BING)
+uk_BING_sentiment <- get_Sentiment(uk_corpus, BING)
 
 # To find the total sentiment we calculated the sum of the sentiment columns that we generated earlier 
 # To achieve this we are using the sum function and then specifying which variable to pull from and using the $ to specify which column to pull from for the sums
-american_total_sentiment = sum(american_sentiment$sentiment)
-uk_total_sentiment = sum(uk_sentiment$sentiment)
+us_total_sentiment = sum(us_BING_sentiment$sentiment)
+uk_total_sentiment = sum(uk_BING_sentiment$sentiment)
 
 #View top 15 contributing words to BING sentiment
-american__word_sentiment <- get_Contributing_Negative_Words(american_corpus)
+us__word_sentiment <- get_Contributing_Negative_Words(us_corpus)
 uk_word_sentiment <- get_Contributing_Negative_Words(uk_corpus)
 
-
-
-
-#Problem E. First we assign the nrc function and run it for the first time to make sure its running smoothly
+#Create our joy and anger word list from the NRC lexicon
 nrc <- lexicon_nrc()
-nrc_joy_us <- nrc%>%
-  filter(sentiment == "joy") # we sort all the joy and anger words into their own variables
-nrc_anger_us <- nrc%>%
+nrc_joy <- nrc%>%
+  filter(sentiment == "joy")
+nrc_anger <- nrc%>%
   filter(sentiment == "anger")
-nrc_joy_us_count <- american_corpus%>%
-  inner_join(nrc_joy_us)%>%
-  count(word, sort = TRUE)%>%
-  top_n(15) # this is to sort the variables to the top 15 words by the number of times the words occur
-nrc_anger_us_count <- american_corpus%>%
-  inner_join(nrc_anger_us)%>%
-  count(word, sort = TRUE)%>%
-  top_n(15)
-
-# Repeat for UK
-nrc_joy_uk <- nrc%>% # this just holds the joy words not actually consisting of the UK joy words because we haven't filtered yet through yet
-  filter(sentiment == "joy") # we sort all the joy and anger words into their own variables
-nrc_anger_uk <- nrc%>%
-  filter(sentiment == "anger")
-nrc_joy_uk_count <- uk_corpus%>%
-  inner_join(nrc_joy_uk)%>%
-  count(word, sort = TRUE)%>%
-  top_n(15)
-nrc_anger_uk_count <- uk_corpus%>%
-  inner_join(nrc_anger_uk)%>%
-  count(word, sort = TRUE)%>%
-  top_n(15)
-
-## This gets the individual occurences of each UK/US Joy/Anger corpus not combing the occurrences together ##
-
-disgustNovel <- nrc%>% #W.I.P
+nrc_disgust <- nrc%>%
   filter(sentiment == "disgust")
 
-  
+#Create the anger and joy count for our two corpus
+us_joy_count <- joy_Count(us_corpus, nrc_joy)
+us_anger_count <- anger_Count(us_corpus, nrc_anger)
+
+uk_joy_count <- joy_Count(uk_corpus, nrc_joy)
+uk_anger_count <- anger_Count(uk_corpus, nrc_anger)
+
+#Find the top disgust novel in each corpus
+top_us_disgust <- get_Top_Disgust(us_corpus, nrc_disgust)
+top_uk_disgust <- get_Top_Disgust(uk_corpus, nrc_disgust)
+
+#Find the AFINN sentiment for each corpus
+AFINN <- lexicon_afinn()
+us_AFINN_sentiment <- get_AFINN_Sentiment(us_corpus, AFINN)
+uk_AFINN_sentiment <- get_AFINN_Sentiment(uk_corpus, AFINN)
 
